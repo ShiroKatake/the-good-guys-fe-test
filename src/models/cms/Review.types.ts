f69e6566d097f4d7b378465d62ae6f538e64ddac
ShiroKatake <tgkhang262@gmail.com>
2024-04-02 19:09:38 +1100

    fix(review cms type): changed to reflect the fields fetched from the cms

--- src/models/cms/Review.types.ts
+++ src/models/cms/Review.types.ts
@@ -1,28 +1,29 @@
-export interface Review {
-  reviewHdrId: number;
-  productId: number;
-  prodNbr: number;
-  reviewId: number;
-  reviewType: string;
-  orderDate: string;
-  submissionDate: string;
-  customerName: string;
-  reviewTitle: string;
-  reviewText: string;
-  rating: number;
-  videoUrl: string;
-  recommendation: string;
-  responseTitle: string;
-  responseText: string;
-  customText: string;
-  published: string;
+export interface ReviewModel {
+  REVIEW_HDR_ID: number;
+  PRODUCT_ID: number;
+  PROD_NBR: number;
+  REVIEW_ID: number;
+  REVIEW_TYPE: string;
+  ORDER_DATE: string;
+  SUBMISSION_DATE: string;
+  CUSTOMER_NAME: string;
+  REVIEW_TITLE: string;
+  REVIEW_TEXT: string;
+  RATING: number;
+  VIDEO_URL: string;
+  RECOMMENDATION: string;
+  RESPONSE_TITLE: string;
+  RESPONSE_TEXT: string;
+  CUSTOM_TEXT: string;
+  PUBLISHED: string;
   objectID: string;
   _highlightResult: {
-    prodNbr: {
+    PROD_NBR: {
       value: string;
       matchLevel: string;
       fullyHighlighted: boolean;
       matchedWords: string[];
-    };
+    }
   };
+
 }
