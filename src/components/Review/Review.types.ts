5008c32af98b2fd48c0df9d966fc3958ca6e12e8
ShiroKatake <tgkhang262@gmail.com>
2024-04-02 17:10:23 +1100

    fix(review type): made unused attributes optional

--- src/components/Review/Review.types.ts
+++ src/components/Review/Review.types.ts
@@ -1,15 +1,16 @@
-// I assume all data is important, but not all are required
-export interface ReviewData {
-  reviewType: string;
-  orderDate: string;
-  submissionDate: string;
+// optional fields won't normally be optional. The brief only
+// requires 4 to display, so they'll stay optional to avoid error messages
+export interface ReviewProps {
+  reviewType?: string;
+  orderDate?: string;
+  submissionDate?: string;
   customerName: string;
   reviewTitle: string;
   reviewText: string;
   rating: number;
-  videoUrl: string;
-  recommendation: boolean;
-  responseTitle: string;
-  responseText: string;
-  customText: string;
+  videoUrl?: string;
+  recommendation?: boolean;
+  responseTitle?: string;
+  responseText?: string;
+  customText?: string;
 }
