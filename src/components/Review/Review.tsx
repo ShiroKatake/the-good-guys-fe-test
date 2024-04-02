ec79f5c34edf5dbbf442906497420eae8f0e6d38
ShiroKatake <tgkhang262@gmail.com>
2024-04-02 19:26:30 +1100

    fix(review component): added styling and help text

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ReviewProps } from "./Review.types";
+import "./Review.styled.css";
 
 export const Review: React.FC<ReviewProps> = ({ rating, reviewTitle, reviewText, customerName }) => {
   return (
-    <div>
-      <div>{rating}</div>
-      <div>{reviewTitle}</div>
-      <div>{reviewText}</div>
-      <div>{customerName}</div>
+    <div className="review">
+      <p>{customerName}</p>
+      <p>Ratings: {rating}</p>
+      <p>{reviewTitle}</p>
+      <p>{reviewText}</p>
+      {/* date, review type, etc. */}
     </div>
   );
 };
