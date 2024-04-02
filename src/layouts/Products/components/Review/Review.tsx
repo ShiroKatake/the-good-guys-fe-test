146ada1a41da2578211cdac5214695fcd9795d51
ShiroKatake <tgkhang262@gmail.com>
2024-04-02 21:40:43 +1100

    fix(review component): conditionally render empty fields

--- src/layouts/Products/components/Review/Review.tsx
+++ src/layouts/Products/components/Review/Review.tsx
@@ -5,10 +5,10 @@ import "./Review.styled.css";
 export const Review: React.FC<ReviewProps> = ({ rating, reviewTitle, reviewText, customerName }) => {
   return (
     <div className="review">
-      <p>{customerName}</p>
+      <p>{customerName ?? "Anonymous"}</p>
       <p>Ratings: {rating}</p>
-      <p>{reviewTitle}</p>
-      <p>{reviewText}</p>
+      {reviewTitle && <p>{reviewTitle}</p>}
+      {reviewText && <p>{reviewText}</p>}
       {/* date, review type, etc. */}
     </div>
   );
