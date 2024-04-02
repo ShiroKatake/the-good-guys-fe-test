39346608be9e26560de6523768098f8560372a7a
ShiroKatake <tgkhang262@gmail.com>
2024-04-02 21:41:28 +1100

    fix(review component): conditionally render empty fields

--- src/layouts/Products/components/Review/Review.types.ts
+++ src/layouts/Products/components/Review/Review.types.ts
@@ -1,13 +1,13 @@
 // optional fields won't normally be optional. The brief only
 // requires 4 to display, so they'll stay optional to avoid error messages
 export interface ReviewProps {
+  customerName: string;
+  rating: number;
+  reviewTitle?: string;
+  reviewText?: string;
   reviewType?: string;
   orderDate?: string;
   submissionDate?: string;
-  customerName: string;
-  reviewTitle: string;
-  reviewText: string;
-  rating: number;
   videoUrl?: string;
   recommendation?: boolean;
   responseTitle?: string;
